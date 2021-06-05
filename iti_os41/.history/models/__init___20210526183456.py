69fbbaf263bea07a98039bf9ebf55461bed4d6e9
Marwa-Medhat <60843644+Marwa-Medhat@users.noreply.github.com>
2021-06-06 00:44:31 +0200

    delete
    delete

--- iti_os41/.history/models/__init___20210526183456.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import medicineModel
-from . import orderLinesModel
-from . import orderModel
-# name of file
-
-# incsv name of model _name
--- iti_os41/.history/wizards/__init___20210530203726.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import wizardorder
-
-# name of file
-
-# incsv name of model _name
